7efb25e8580bd36735bbffabceabf749afe75e39
lanasiderae <swietlana.moroz@gmail.com>
2025-06-29 22:17:26 -0500

    v2.2.3

--- .github/workflows/semgrep-format.sh
+++ .github/workflows/semgrep-format.sh
@@ -3,30 +3,30 @@
 jq -r '
   .results[]
   | "\(.path):\(.start.line):\(.start.col // 1): \(
-      if .extra.severity == \"CRITICAL\" then \"error\"
-      elif .extra.severity == \"ERROR\" then \"error\"
-      elif .extra.severity == \"WARNING\" then \"warning\"
-      else \"info\"
+      if .extra.severity == "CRITICAL" then "error"
+      elif .extra.severity == "ERROR" then "error"
+      elif .extra.severity == "WARNING" then "warning"
+      else "info"
     end
   ): \(
-      if .extra.severity == \"CRITICAL\" then \"🚨 [CRITICAL]\"
-      elif .extra.severity == \"ERROR\" then \"🔴 [HIGH]\"
-      elif .extra.severity == \"WARNING\" then \"⚠️ [MEDIUM]\"
-      else \"💡 [LOW]\"
+      if .extra.severity == "CRITICAL" then "🚨 [CRITICAL]"
+      elif .extra.severity == "ERROR" then "🔴 [HIGH]"
+      elif .extra.severity == "WARNING" then "⚠️ [MEDIUM]"
+      else "💡 [LOW]"
     end
   ) \(
     if .extra.metadata.source? != null then
-      \"<[\(.check_id)](\(.extra.metadata.source))>\"
+      "<[\(.check_id)](\(.extra.metadata.source))>"
     else
-      \"<[\(.check_id)](https://semgrep.dev/r/\(.check_id))>\"
+      "<[\(.check_id)](https://semgrep.dev/r/\(.check_id))>"
     end
   ) \(
-    (.extra.message | gsub(\"\n\"; \" \"))
+    (.extra.message | gsub("\n"; " "))
     + (
         if (.extra.metadata.references | length) > 0 then
-          \"\n📚 Docs: \" + (.extra.metadata.references | join(\", \"))
+          "\n📚 Docs: " + (.extra.metadata.references | join(", "))
         else
-          \"\"
+          ""
         end
       )
   )"
